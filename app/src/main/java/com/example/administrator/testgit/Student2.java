f7a0bff6696335d939c0b46509f8fe02982ee3b7
panjunpeien <panjunpeien@163.com>
2018-01-18 22:42:29 +0800

    新建一个类Student2 提交

---
+++ app/src/main/java/com/example/administrator/testgit/Student2.java
@@ -0,0 +1,20 @@
+package com.example.administrator.testgit;
+
+/**
+ * Created by Administrator on 2018/1/18.
+ */
+
+public class Student2 {
+    private String name;
+
+    public Student2() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
