2744bc668b4e9a1e77e67836e747cb1df833daf9
panjunpeien <panjunpeien@163.com>
2018-01-20 21:00:55 +0800

    创建分支  branch01  提交

--- app/src/main/java/com/example/administrator/testgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testgit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.administrator.testgit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //修改文件之后的提交
+        Log.i("SunChunYue--->", "AAA");
+        ((TextView) findViewById(R.id.text)).setText("SunChunYue");
     }
 }
