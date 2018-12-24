bf83c26a1f7e21c8d72fddbbf18ae539adb7e20e
panjun <panjun@lattebank.com>
2018-12-24 19:20:39 +0800

    Test 测试2018/1224/1920

--- app/src/main/java/com/example/administrator/testgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testgit/MainActivity.java
@@ -12,11 +12,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //修改文件之后的提交
+
         Log.i("SunChunYue--->", "AAA");
         ((TextView) findViewById(R.id.text)).setText("SunChunYue");
         //版本 2.0
         //版本 2.1
         //版本 2.2
         //创建branch02，并且合并到master
+        //Test 2018/1224/1917
     }
 }
