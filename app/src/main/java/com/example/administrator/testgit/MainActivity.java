ee915089e57a8369aabc19e0f8cf1166588bec6d
panjunpeien <panjunpeien@163.com>
2019-01-12 17:57:32 +0800

    add gitignore

--- app/src/main/java/com/example/administrator/testgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testgit/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         //修改文件之后的提交
         //
+        //
 
         Log.i("SunChunYue--->", "AAA");
         ((TextView) findViewById(R.id.text)).setText("SunChunYue");
